70c34ff5475476fa6386e997adc1054867fb58b3
Clint <clintsiphelelembambo@gmail.com>
2024-10-28 21:46:28 +0200

    initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using FashionFoundry.Models;
+
+namespace FashionFoundry.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<FashionFoundry.Models.Item> Item { get; set; } = default!;
+    }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FashionFoundry.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Image")]
+        public string? ImageUrl { get; set; }
+
+        [Display(Name = "Description")]
+        public string? Description { get; set; }
+
+        [Display(Name = "Price")]
+        public decimal Price {  get; set; }
+    }
+}
